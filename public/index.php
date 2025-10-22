0e7a9c07736a000f49b89fc6066fd2edb02cedc7
Ulysse Pennetier <upennetier@gmail.com>
2025-10-22 18:20:28 +0200

    feat(CORS): CORS policy

--- public/index.php
+++ public/index.php
@@ -4,7 +4,17 @@
   ini_set('display_startup_errors', 1);
   error_reporting(E_ALL);
   // DEV //
-  
+
+  header("Access-Control-Allow-Origin: *"); 
+  header("Access-Control-Allow-Methods: GET, OPTIONS");
+  header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With"); 
+  header("Access-Control-Max-Age: 86400"); 
+
+  if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(204);
+    exit();
+  }
+
   require __DIR__ . '/../vendor/autoload.php';
   require __DIR__ . '/../bootstrap.php';
   require_once __DIR__ . '/../src/core/Router.php';
